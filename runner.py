765ada541c5593d7ed732b5b4cb156b785e43456
V <veronika.m.winters@gmail.com>
2023-08-29 10:17:25 -0700

    cleanups

--- runner.py
+++ runner.py
@@ -6,14 +6,9 @@ import os
 def run_module(path):
     bytes = open(path, "rb").read()
     store = Store(engine.Universal(Compiler))
-
     module = Module(store, bytes)
     wasi_version = wasi.get_version(module, strict=True)
-
     wasi_env = wasi.StateBuilder("").finalize()
-
     import_object = wasi_env.generate_import_object(store, wasi_version)
-
     instance = Instance(module, import_object)
-
     instance.exports._start()
